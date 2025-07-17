bcb8617a8cac175b90e0d1c2ca11fafa1569e33f
mybestnickname <25727207+mybestnickname@users.noreply.github.com>
2025-07-17 09:26:06 +0300

    added start of scheduled checks

--- app/api/app.py
+++ app/api/app.py
@@ -35,6 +35,43 @@ def create_app() -> FastAPI:
     # Сохраняем настройки в app.state
     app.state.config = Config()
 
+    # доступные проверки храним в app.state.checks
+    if getattr(app.state.config, 'checks', None) is None:
+        app.state.checks = {}
+    checks_in_config = {check.name: check for check in app.state.config.checks}
+
+    check_services_with_cfg = {}
+    for check_class in find_check_subclasses(resolve_path(CHECKS_DIR), Check):
+        try:
+            check_services_with_cfg[check_class.name] = check_class(
+                app.state.config.collector,
+                checks_in_config[check_class.name].config,
+                checks_in_config[check_class.name].metadata,
+                checks_in_config[check_class.name].timeout
+            )
+        except KeyError:
+            continue
+
+    app.state.checks = check_services_with_cfg
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        # STARTUP
+        fi_service = app.state.checks.get('file_integrity')
+        if fi_service:
+            fi_service_afick_throttler = fi_service.afick_service.throttler
+            await fi_service_afick_throttler.start_workers()
+
+        await run_checks(app.state.config.checks, app.state.checks)
+
+        yield
+
+        # SHUTDOWN
+        fi_service = app.state.checks.get('file_integrity')
+        if fi_service:
+            fi_service_afick_throttler = fi_service.afick_service.throttler
+            fi_service_afick_throttler.shutdown()
+
     app.openapi = lambda: custom_openapi(app)
 
     app.include_router(router.router, prefix='/nodewatch/v1alpha1')
